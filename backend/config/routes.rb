cadcc672a3b567a9ccdb9180f52551b962a48bd2
Kana Oota <oota@sjc-inc.co.jp>
2024-09-17 08:52:52 +0900

    スキルの表示

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'name/show'
   get 'user_history/list'
+  get 'skill/list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- frontend/src/portfolio.js
+++ frontend/src/portfolio.js
@@ -4,6 +4,7 @@ import './portfolio.css'; // スタイルシートをインポートします。
 function Portfolio() {
   const [name, setName] = useState('');
   const [experience, setExperience] = useState([]);
+  const [skills, setSkill] = useState([]);
 
   useEffect(() => {
     // 名前を取得
@@ -31,17 +32,32 @@ function Portfolio() {
       })
       .then(data => setExperience(data.user_histories))
       .catch(error => console.error('Error fetching experience:', error));
+
+      // スキルリストを取得
+      fetch('http://localhost:3000/skill/list', {
+        credentials: 'include' // 必要に応じてクッキーを送信する場合
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => setSkill(data.skills))
+        .catch(error => console.error('Error fetching experience:', error));
   }, []);
 
   return (
     <div className="Portfolio">
       <header className="Portfolio-header">
+{/* TODO メニューを追加する
         <div className='menu'>
           <a href='#name'>名前</a>
           <a href="#experience">業務経歴</a>
           <a href="#skills">スキル</a>
           <a href="#works">成果物</a>
         </div>
+ */}
       </header>
 
       <section id="name" className="Portfolio-section">
@@ -61,13 +77,21 @@ function Portfolio() {
 
       <section id="skills" className="Portfolio-section">
         <h3>スキル</h3>
-        <p>[スキルリスト]</p>
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={index}>
+              <strong>{skill.name}</strong>
+            </li>
+          ))}
+        </ul>
       </section>
-
+{/* TODO： 現時点では業務経歴と類似しかなさそうなので、成果物は一旦削除
       <section id="works" className="Portfolio-section">
         <h3>成果物</h3>
-        <p>[成果物]</p>
+        <p>[成果物リスト]</p>
       </section>
+ */}
+
     </div>
   );
 }
